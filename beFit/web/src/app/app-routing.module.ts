c8c122acd4ffb27e1d6083f5f15cb531eb2b0b2c
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-11-25 21:48:36 +0200

    Updated footer/navbar. Added two types of layouts.  Updated style for auth/reg pages.

--- beFit/web/src/app/app-routing.module.ts
+++ beFit/web/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppLayoutComponent } from './web/shared/layout/app-layout/app-layout.component';
+import { AuthLayoutComponent } from './web/shared/layout/auth-layout/auth-layout.component';
+
 // localhost:4200/
 const routes: Routes = [
   {
@@ -10,26 +13,32 @@ const routes: Routes = [
   },
   {
     path: 'home',
+    component: AppLayoutComponent,
     loadChildren: () => import('./web/home/home.module').then((m) => m.HomeModule)
   },
   {
     path: 'inregistrare',
+    component: AuthLayoutComponent,
     loadChildren: () => import('./web/autorizare/inregistrare/inregistrare.module').then((m) => m.InregistrareModule)
   },
   {
     path: 'autentificare',
+    component: AuthLayoutComponent,
     loadChildren: () => import('./web/autorizare/autentificare/autentificare.module').then((m) => m.AutentificareModule)
   },
   {
     path: 'abonamente',
+    component: AppLayoutComponent,
     loadChildren: () => import('./web/abonamente/abonamente.module').then((m) => m.AbonamenteModule)
   },
   {
     path: 'profil',
+    component: AppLayoutComponent,
     loadChildren: () => import('./web/profil/profil.module').then((m) => m.ProfilModule)
   },
   {
     path: 'admin',
+    component: AppLayoutComponent,
     loadChildren: () => import('./web/admin/admin.module').then((m) => m.AdminModule)
   },
 ];
--- beFit/web/src/app/web/autorizare/autentificare/autentificare.module.ts
+++ beFit/web/src/app/web/autorizare/autentificare/autentificare.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { AutentificareRoutingModule } from './autentificare-routing.module';
 import { AutentificareComponent } from './autentificare/autentificare.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 
 @NgModule({
   declarations: [AutentificareComponent],
   imports: [
     CommonModule,
-    AutentificareRoutingModule
+    AutentificareRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class AutentificareModule { }
