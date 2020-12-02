f2fd1ea6e605b48cb3de14f557b516804d7d809d
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-12-02 16:59:52 +0200

    Updated abonamente/detalii_abonament pages.

--- beFit/web/src/app/web/abonamente/detalii/detalii.component.ts
+++ beFit/web/src/app/web/abonamente/detalii/detalii.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { ConfirmationDialogService } from '../../shared/components/dialog/dialog.service';
+import { Router } from '@angular/router';
+
+import { AbonamentModel } from '../../../services/models/abonament/abonament.model';
+
+import * as data from '../../../../assets/static.data.json';
 
 @Component({
   selector: 'app-detalii',
@@ -7,17 +12,35 @@ import { ConfirmationDialogService } from '../../shared/components/dialog/dialog
   styleUrls: ['./detalii.component.css']
 })
 export class DetaliiComponent implements OnInit {
-  constructor(private confirmationDialogService: ConfirmationDialogService) { }
+
+  public details: AbonamentModel;
+
+  constructor(
+    private confirmationDialogService: ConfirmationDialogService,
+    private router: Router,
+  ) {
+
+    const reqId= this.router.url.split('/').slice(-1)[0];
+    console.log(reqId);
+    this.details = data.detalii_abonament;
+
+  }
 
   public openDialog() {
-    this.confirmationDialogService.confirm('', 'Activezi acest abonament?')
-    .then((confirmed) => {
-      console.log('Button:', confirmed);
-    })
-    .catch(() => {
-      console.log('Dismiss');
-    });
+    this.confirmationDialogService.confirm('', `Activezi "${this.details.title}" ?`)
+      .then((confirmed) => {
+        if (confirmed) {
+          console.log('Se cumpără!!!');
+        }
+        else {
+          console.log('No tengo dinero, baws! :(');
+        }
+      })
+      .catch(() => {
+        console.log('Am apasat din greseala!');
+      });
   }
+
   ngOnInit(): void {
   }
 
